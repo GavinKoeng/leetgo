27e9d4736c44c161e60bd0d16ff7a8d743e2226f
gavin <gavin.cgq@gmail.com>
2026-08-08 20:07:47 +0800

    add the solution 1 of 20 in cpp

--- cpp/0020.valid-parentheses/solution.cpp
+++ cpp/0020.valid-parentheses/solution.cpp
@@ -1,9 +1,9 @@
-// Created by Gavin at 2026/01/25 11:04
-// leetgo: 1.4.15
+// Created by gavin at 2026/08/08 17:52
+// leetgo: 1.4.17
 // https://leetcode.cn/problems/valid-parentheses/
 
 #include <bits/stdc++.h>
-#include "LC_IO.h"
+#include "../LC_IO.h"
 using namespace std;
 
 // @lc code=begin
@@ -11,21 +11,20 @@ using namespace std;
 class Solution {
 public:
     bool isValid(string s) {
-        int n = s.size();
-        if (n % 2 == 1) {
-            return false;
-        }
-
-        unordered_map<char, char> pairs = { {')', '('}, {'}', '{'}, {']', '['} };
         stack<char> stk;
-        for (char ch : s) {
-            if (pairs.count(ch)) {
-                if (stk.empty() || stk.top() != pairs[ch]) {
-                    return false;
-                }
+        for (int i = 0; i < s.length(); i++) {
+            if (s[i] == '(' || s[i] == '{' || s[i] == '[') {
+                stk.push(s[i]);
+            } else if (stk.empty()) {
+                return false;
+            } else if (s[i] == ')' && stk.top() != '(') {
+                return false;
+            } else if (s[i] == '}' && stk.top() != '{') {
+                return false;
+            } else if (s[i] == ']' && stk.top() != '[') {
+                return false;
+            } else if ((s[i] == ')' && stk.top() == '(') || (s[i] == '}' && stk.top() == '{') || (s[i] == ']' && stk.top() == '[')) {
                 stk.pop();
-            } else {
-                stk.push(ch);
             }
         }
         return stk.empty();
